7bf53193c20e3da65c87d4681f44502e4153bf8d
René Martínez <hola@r3n0.com>
2023-11-09 15:09:25 -0500

    updates

--- clases/Particula.js
+++ clases/Particula.js
@@ -6,7 +6,27 @@ class Particula {
 		this.side = Math.ceil(random(3, 15));
 	}
 	update() {
-		this.r = random();
+		this.r = random(100);
+
+		if (this.r < 25) {
+			// hacia arriba
+			this.pos.y -= this.side;
+		} else if (this.r < 50) {
+			// hacia la derecha
+			this.pos.x += this.side;
+		} else if (this.r < 75) {
+			// hacia abajo
+			this.pos.y += this.side;
+		} else {
+			// hacia izquierda
+			this.pos.x -= this.side;
+		}
+
+		this.lifespan -= 1;
+
+		if (this.lifespan <= 0) {
+			this.isAlive = false;
+		}
 	}
 	display() {
 		square(this.pos.x, this.pos.y, this.side);
